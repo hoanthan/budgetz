1e9eae24156dae656df1303b411fd821fbffe31d
Hoan Than <hoanthan317@gmail.com>
2025-02-18 10:45:43 +0700

    ui updates

--- app/routes/plans/detail/components/PlanSummary.tsx
+++ app/routes/plans/detail/components/PlanSummary.tsx
@@ -3,6 +3,12 @@ import { MoveDownLeft, MoveUpRight } from "lucide-react";
 import { useMemo } from "react";
 import { Tables } from "supabase/database.types";
 import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "~/components/ui/accordion";
+import {
   Collapsible,
   CollapsibleContent,
   CollapsibleTrigger,
@@ -57,8 +63,65 @@ const PlanSummary: React.FC<{
   }, [actualInAmount, actualOutAmount]);
 
   return (
-    <Collapsible className="sticky top-0 left-0 py-2 bg-white z-[1] pr-8 flex flex-col gap-1">
-      <CollapsibleTrigger asChild>
+    <Accordion
+      type="multiple"
+      className="sticky top-0 left-0 py-2 bg-white z-[1]"
+    >
+      <AccordionItem value="estimated" className="border-0">
+        <AccordionTrigger className="w-full py-1 items-center">
+          <div className="w-full flex justify-between items-center text-xs">
+            <p className="text-muted-foreground">Estimated</p>
+            <p
+              className={cn({
+                "text-destructive": estimatedBalance < 0,
+                "text-green-600": estimatedBalance > 0,
+              })}
+            >
+              <Currency>{estimatedBalance}</Currency>
+            </p>
+          </div>
+        </AccordionTrigger>
+        <AccordionContent>
+          <div className="flex flex-col items-end text-xs pr-8">
+            <p className="text-green-600 flex gap-1 items-center">
+              <MoveDownLeft size={12} />{" "}
+              <Currency style="decimal">{estimatedInAmount}</Currency>
+            </p>
+            <p className="text-destructive flex gap-1 items-center">
+              <MoveUpRight size={12} />{" "}
+              <Currency style="decimal">{estimatedOutAmount}</Currency>
+            </p>
+          </div>
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="actual" className="border-0">
+        <AccordionTrigger className="w-full py-1 items-center">
+          <div className="w-full flex justify-between items-center text-xs">
+            <p className="text-muted-foreground">Actual</p>
+            <p
+              className={cn({
+                "text-destructive": actualBalance < 0,
+                "text-green-600": actualBalance > 0,
+              })}
+            >
+              <Currency>{actualBalance}</Currency>
+            </p>
+          </div>
+        </AccordionTrigger>
+        <AccordionContent>
+          <div className="flex flex-col items-end text-xs pr-8">
+            <p className="text-green-600 flex gap-1 items-center">
+              <MoveDownLeft size={12} />{" "}
+              <Currency style="decimal">{actualInAmount}</Currency>
+            </p>
+            <p className="text-destructive flex gap-1 items-center">
+              <MoveUpRight size={12} />{" "}
+              <Currency style="decimal">{actualOutAmount}</Currency>
+            </p>
+          </div>
+        </AccordionContent>
+      </AccordionItem>
+      {/* <CollapsibleTrigger asChild>
         <div className="grid grid-cols-2 text-xs" role="button">
           <div>
             <p className="text-muted-foreground">Estimated</p>
@@ -107,8 +170,8 @@ const PlanSummary: React.FC<{
             </p>
           </div>
         </div>
-      </CollapsibleContent>
-    </Collapsible>
+      </CollapsibleContent> */}
+    </Accordion>
   );
 };
 
